e88bfd84ef0685d3afd7df3a0547b9db03a044fa
meenakshy <meenakshy3602@gmail.com>
2025-12-11 11:24:04 +0530

    subscriptions

---
+++ cineflix/subscriptions/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+
+from multiselectfield import MultiSelectField
+
+# Create your models here.
+
+from movies.models import BaseClass
+
+class DeviceChoices(models.TextChoices):
+
+    ALL='All Devices','All Devices'
+
+    PHONE='Phone','Phone'
+
+    TABLET='Tablet','Tablet'
+
+    TV='TV','TV'
+
+    LAPTOP='Laptop','Laptop'
+
+class QualityChoices(models.TextChoices):
+
+    P480='480 p','480 p'
+
+    P1080='Upto 1080 p','Upto 1080 p'
+
+    P4K='Upto 4k','Upto 4k'
+
+class ScreenOrDownloadDeviceChoices(models.IntegerChoices):
+
+    ONE= 1,'1'
+
+    TWO= 2,'2'
+
+    FOUR=4,'4'
+
+class SubscriptionPlans(BaseClass):
+
+    name=models.CharField(max_length=25)
+
+    amount=models.FloatField()
+
+    devices=MultiSelectField(choices=DeviceChoices.choices)
+
+    quality=models.CharField(max_length=30,choices=QualityChoices.choices)
+
+    no_of_screens=models.IntegerChoices(choices=ScreenOrDownloadDeviceChoices.choices)
+
+    download_devices=models.IntegerChoices(choices=ScreenOrDownloadDeviceChoices.choices)
+
+    class Meta:
+
+        verbose_name='Subscription Plans'
+
+        verbose_name_plural='Subscription Plans'
+
+        def __str__(self):
+
+            return self.name
+
+
